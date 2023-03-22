42a93db9e7bd76349cbc14caff429fc00e538b08
TJ Kim <taejin.kim96@gmail.com>
2023-03-22 14:09:36 -0400

    .

--- classes/Server.py
+++ classes/Server.py
@@ -19,7 +19,7 @@ class Server():
 
         return awarded, rewards, wait_times
 
-    def serve_users(self, usr_list):
+    def serve_users(self, usr_list, mu_bar = None, move_probs = None, rsv_flag = False):
         
         load = len(usr_list)
         self.load_history += [load]
@@ -41,12 +41,35 @@ class Server():
             awarded[u] = True
         for u in usr_list:
             rewards[u] = int(np.random.rand() < self.mu[u, self.s_idx])
+            
+        if rsv_flag:
+            waittimes = self.coorindate_users(usr_list, mu_bar, move_probs)
         
         return awarded, rewards, overflow_flag, waittimes
     
-    def coorindate_users(self, usr_list, move_probs):
+    def coorindate_users(self, usr_list, mu_bar, move_probs):
         
         # Find lowest n users and ban them for some time
+        num_users = self.mu.shape[0]
+        waittimes = np.zeros(num_users)
+        num_kick = int(max(0, len(usr_list) - self.cap))
+        compare_list = np.zeros(len(usr_list))
         
+        if num_kick > 0:
+            for i in range(len(usr_list)):
+                compare_list[i] = mu_bar[i] * move_probs[i]
+            
+            # decide who to ban 
+            idx_kick = np.argpartition(compare_list, num_kick)[:num_kick]
+            idx_kick_sort = idx_kick[np.argsort(compare_list[idx_kick])]
+            
+            # decide on how long to ban
+            idx_good = np.argpartition(compare_list, - self.cap)[-self.cap:]
+            idx_good_sorted = idx_good[np.argsort(-compare_list[idx_good])]
+            
+            p_sub = move_probs[idx_good_sorted]
+            waittime = np.ceil((1 - np.prod(p_sub))**(-1))
+            
+            waittimes[usr_list[idx_kick_sort]] = waittime
         
-        return awarded, rewards, overflow_flag, waittimes
+        return waittimes
--- tools/sim_utils.py
+++ tools/sim_utils.py
@@ -19,41 +19,54 @@ def gen_eq_locs(space_1d, nums, offset = 0.5):
 def dist_usr_arms(users):
     # select arms for all users and return vector of server to arm 
     
-    lrn_x = np.zeros([len(users), users[0].num_servers])
+    lrn_x = np.zeros([len(users), users[0].num_servers + 1])
     
     for u in range(len(users)):
         arm_id = users[u].select_arm_UCB2()
         lrn_x[u,arm_id] = 1
     
-    return lrn_x
+    return lrn_x 
 
-def dist_receive_rewards(servers, users, lrn_x_dist):
+def dist_receive_rewards(servers, users, lrn_x_dist, rsv_flag = False): # add moveprobs and mu_bar
     # Obtain rewards and update user UCB records from servers
     
     # Per server accept list
     usr_list_dict = {}
-    mu_list_dict = {}
+    mubar_list_dict = {}
+    prob_list_dict = {}
     
     # Organize user list for each server
     for s in range(len(servers)):
         usr_list_dict[s] = []
+        mubar_list_dict[s] = []
+        prob_list_dict[s] = []
         for u in range(len(users)):
             if lrn_x_dist[u,s] > 0:
                 usr_list_dict[s] += [u]
+                mubar_list_dict[s] += [users[u].means[s]]
+                prob_list_dict[s] += [users[u].P_loc[users[u].usr_place,users[u].usr_place]]
 
     # Let each server process user
     awarded = np.zeros_like(servers[0].mu)
     rewards = np.zeros_like(servers[0].mu)
     overflow_flag = np.zeros_like(servers[0].mu)
+    waittimes = np.zeros_like(servers[0].mu)
     
     for s in range(len(servers)):
-        awarded[:,s], rewards[:,s], overflow_flag[:,s] = servers[s].serve_users(usr_list_dict[s])
+        awarded[:,s], rewards[:,s], overflow_flag[:,s], waittimes[:,s] = servers[s].serve_users(usr_list_dict[s],
+            mu_bar = mubar_list_dict[s], move_probs = prob_list_dict[s], rsv_flag = rsv_flag)
+        
+    awarded = np.append(awarded, np.zeros([len(users),1]), axis = 1)
+    rewards = np.append(rewards, np.zeros([len(users),1]), axis = 1)
+    overflow_flag = np.append(overflow_flag, np.zeros([len(users),1]), axis = 1)
+    waittimes = np.append(waittimes, np.zeros([len(users),1]), axis = 1)
         
     # Update each user's history based on server response
     for u in range(len(users)):
         arm_id = np.argmax(lrn_x_dist[u])
         reward = rewards[u,arm_id]
-        users[u].receive_reward(arm_id, reward)
+        waittime = waittimes[u,arm_id]
+        users[u].receive_reward(arm_id, reward, waittime)
     
     return
 
